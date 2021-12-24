f97ec53da9fcac3b9aa60c58ac447ece1d8dd9db
Minsik Kim <kmsbernard@gmail.com>
2021-12-24 17:05:48 +0900

    override save as

--- index.js
+++ index.js
@@ -32,17 +32,7 @@ const DEFAULT_PDF = {
  * @param  {string} filename
  */
 const saveAs = (uri, filename) => {
-    const link = document.createElement('a');
-
-    if (typeof link.download === 'string') {
-        link.href = uri;
-        link.download = filename;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    } else {
-        window.open(uri);
-    }
+    window.open(uri);
 };
 
 /**
